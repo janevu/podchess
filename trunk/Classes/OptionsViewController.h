ffc73d3d147908228385e61296c24fa305556f11
Sakur.deaGod <Sakur.deaGod@836fd814-8a2e-11de-8f56-7b2e0289298d>
2009-09-25 07:43:05 +0000

    Add AI selection UI Refactor the current Setting page ( use two level of setting pages) AI selection now works only on UI , should be able to select the actual AI engine soon

--- trunk/Classes/OptionsViewController.h
+++ trunk/Classes/OptionsViewController.h
@@ -17,44 +17,13 @@
  *  along with PodChess.  If not, see <http://www.gnu.org/licenses/>.      *
  ***************************************************************************/
 
+#import <UIKit/UIKit.h>
 
-//
-//  PodChessAppDelegate.m
-//  PodChess
-//
 
-#import "PodChessAppDelegate.h"
+@interface OptionsViewController : UITableViewController {
+    IBOutlet UITableViewCell *generic_options;
+    IBOutlet UITableViewCell *AI_selections;
 
-@implementation PodChessAppDelegate
-
-@synthesize window;
-@synthesize navigationController;
-
-- (void)applicationDidFinishLaunching:(UIApplication *)application {    
-
-    // Override point for customization after application launch
-    //set default preferences
-    float setting = [[NSUserDefaults standardUserDefaults] floatForKey:@"difficulty_setting"];
-    if(setting < 3.0f || setting > 64.0f)
-        [[NSUserDefaults standardUserDefaults] setFloat:7.0f forKey:@"difficulty_setting"];
-    setting = [[NSUserDefaults standardUserDefaults] floatForKey:@"time_setting"];
-    if(setting < 30.0f || setting > 120.0f) {
-        [[NSUserDefaults standardUserDefaults] setFloat:60.0f forKey:@"time_setting"];
-        //this might be the first time run
-        [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"ToggleSound"];
-        [[NSUserDefaults standardUserDefaults] setObject:@"AI_xqwlight" forKey:@"AI"];
-    }
-    
-    [window addSubview:[navigationController view]];
-    navigationController.navigationBarHidden = YES;
-    [window makeKeyAndVisible];
 }
-
-
-- (void)dealloc {
-    [window release];
-    [super dealloc];
-}
-
 
 @end
